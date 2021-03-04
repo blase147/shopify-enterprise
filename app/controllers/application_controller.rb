86d87d28b7a3b882b25e4311810159a506706655
Jacky <jackyinvn@gmail.com>
2021-03-04 22:25:24 +0700

    Fix proxy for subscriptions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,30 @@
 class ApplicationController < ActionController::Base
   before_action :set_hmac
 
+  helper_method :current_setting
+
+  def current_setting
+    return @current_setting if @current_setting.present?
+    @current_setting = current_shop.setting
+
+    if @current_setting.nil?
+      @current_setting = current_shop.create_setting( payment_retries: 3,
+                                                      payment_delay_retries: 1,
+                                                      cancel_enabled: true,
+                                                      pause_resume: true,
+                                                      attempt_billing: false,
+                                                      skip_payment: true,
+                                                      show_after_checkout: false,
+                                                      email_after_checkout: true )
+    end
+
+    @current_setting
+  end
+  
   def set_hmac
     session[:hmac] ||= params[:hmac]
     session[:shop] ||= params[:shop]
     session[:session] ||= params[:session]
   end
 end
+
--- app/helpers/app_proxy_helper.rb
+++ app/helpers/app_proxy_helper.rb
@@ -2,4 +2,8 @@ module AppProxyHelper
   def proxy_path controller, options={}
     "/a/chargezen_production/#{controller}?customer_id=#{params[:customer_id]}&#{options}"
   end
+
+  def icon_tag icon
+    image_tag "icons/#{icon}.svg"
+  end
 end
