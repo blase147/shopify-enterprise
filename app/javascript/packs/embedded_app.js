1a6bb809874b9dd81b1ed16099ef6abe325a1f84
Khurram <soft.qureshi@gmail.com>
2021-04-09 17:01:27 +0500

    fixed carousal design

--- app/javascript/packs/embedded_app.js
+++ app/javascript/packs/embedded_app.js
@@ -101,14 +101,8 @@ $(document).ready(function () {
     slideSpeed: 300,
     paginationSpeed: 400,
     singleItem: true,
-
-    // "singleItem:true" is a shortcut for:
+    dots: false,
     items: 1
-    // itemsDesktop : false,
-    // itemsDesktopSmall : false,
-    // itemsTablet: false,
-    // itemsMobile : false
-
   });
 
 });
