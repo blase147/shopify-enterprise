61f8704702c7d432ee9968bb15f86de1528d0719
Khurram <soft.qureshi@gmail.com>
2021-04-21 20:23:40 +0500

    created edit sms page

--- app/javascript/components/smarty/SmartyMessage/EditSmartyMessage.js
+++ app/javascript/components/smarty/SmartyMessage/EditSmartyMessage.js
@@ -1,12 +1,100 @@
-import React from 'react'
+import {
+  Card,
+  Layout,
+
+  TextField
+} from '@shopify/polaris';
+import React, { useState } from 'react';
 
 const EditSmartyMessage = ({id,handleClose}) => {
+
+    const orderOptions = [
+        { label: "Order By Title", value: 'title' },
+        { label: "Order By Name", value: 'name' }
+    ]
+    const [searchValue, setSearchValue] = useState("");
+    const [order, setOrder] = useState("title");
     return (
-        <div>
-            Edit Smarty SMS of id {id}
-            <br/>
-            <button onClick={()=>handleClose()} >Back</button>
-        </div>
+        <Layout>
+        <Card>
+          <Card.Section>
+            <div className="smarty-sms">
+              <p className="customize-text">Message Custom Keywords</p>
+              <p className="customize-text" style={{ fontWeight: 'normal' }}>Edit the SMS message body to your own style.</p>
+              <form class="">
+                <div className="message-form">
+                  <div class="edit-example" >
+                    <div className="title-input">
+                      <p>Title *</p>
+                      <TextField
+                          placeholder="Account Settings - Options"
+                          value={searchValue}
+                          // error={}
+                          onChange={(value) => setSearchValue(value)}
+                      />
+                      <div className="tick-icon">
+                        <svg width="15" height="13" viewBox="0 0 15 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                          <path d="M0.640625 7.25964C0.677855 7.22695 1.06656 7.42788 1.1067 7.44556C1.50145 7.61915 1.88364 7.82019 2.25012 8.04752C3.19914 8.62831 3.99493 9.42829 4.57071 10.3803C5.03923 11.1605 5.39396 12.6103 6.52644 12.6103C7.06895 12.6103 7.68558 12.6279 8.0979 12.214C8.6019 11.7076 8.61097 10.9684 8.76676 10.3146C9.5387 7.23091 11.0129 4.36746 13.074 1.9474C13.2006 1.79603 13.3292 1.64618 13.4595 1.49796C13.7242 1.20047 14.005 0.917757 14.3006 0.650982C14.3408 0.614566 14.4103 0.53999 14.4933 0.454826C14.5693 0.377342 14.5909 0.26158 14.548 0.161873C14.5052 0.062167 14.4063 -0.00182194 14.2977 3.95519e-05C12.5943 0.0541393 8.5414 0.945797 6.25885 7.79471C6.25885 7.79471 2.78088 5.38686 0.640625 7.25964Z" fill="#57B744"/>
+                        </svg>
+                      </div>
+
+                    </div>
+                    <div className="discription-input">
+                      <p>Description</p>
+                      <TextField
+                          // placeholder="Message’s Keyword"
+                          value={searchValue}
+                          // error={}
+                          onChange={(value) => setSearchValue(value)}
+                      />
+                    </div>
+                  </div>
+                </div>
+              </form>
+          </div>
+
+          </Card.Section>
+        </Card>
+        
+        <Card>
+          <Card.Section>
+            <p className="variable-text">Variables</p>
+            <div className="variable-section">
+              <div className="variables">
+                <p>A set of custom variables is provided to make your message more personal and informative for your customers. 
+                  To use variables is quite simple, you need to select from the available variables set and include it to your 
+                  message body using double brackets: <span style={{color:"red"}}>{'{{variable_goes_here}}'} </span>.
+                </p>
+
+                <div className="available-variables">
+                  <p className="">Available variables</p>
+                  <button>shop_name</button>
+                  <button>shop_domain</button>
+                  <button>shop_email</button>
+                  <button>shop_phone</button>
+                  <button>first_name</button>
+                  <button>last_name</button>
+                  <br></br>
+                  <button>email</button>
+                  <button>phone</button>
+                  <button>features_options</button>
+                </div>
+              </div>
+              <div className="var-auto-complition">
+                <TextField
+                  multiline={4}
+                  placeholder="Hi please reply with a number to modify your account."
+                  value={searchValue}
+                  // error={}
+                />
+                <p>Type @ to have the variables auto-completion.</p>
+
+              </div>
+            </div>
+          </Card.Section>
+        </Card>
+
+      </Layout>
     )
 }
 
