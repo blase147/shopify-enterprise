d0e613401f2a79f761759b3f83580cf99685065b
Khurram <soft.qureshi@gmail.com>
2021-04-23 14:40:58 +0500

    fix for churn rate

--- app/graphql/queries/fetch_dashboard_report.rb
+++ app/graphql/queries/fetch_dashboard_report.rb
@@ -10,8 +10,8 @@ module Queries
       current_month_subscriptions = in_period_subscriptions(subscriptions, (Date.today - 1.month..Date.today))
       subcription_month_revenue = current_month_subscriptions.sum { |subscription| get_orders_total_amount(subscription) }
       active_subscriptions_count = get_subscriptions_count(subscriptions, 'ACTIVE')
-      churn_rate = get_churn_rate(subscriptions, subscriptions_in_period)
-      customer_lifetime = get_customer_lifetime_value(subscriptions_in_period) / churn_rate
+      churn_rate = get_churn_rate(subscriptions, range)
+      customer_lifetime = get_customer_lifetime_value(subscriptions_in_period) / churn_rate rescue 0
       revenue_churn = month_graph_data(subscriptions, range, :revenue_churn_by_date)
       arr_data = year_graph_data(subscriptions, range, :arr_data_by_date)
       mrr_data = month_graph_data(subscriptions, range, :mrr_data_by_date)
@@ -19,7 +19,9 @@ module Queries
       sales_data =  month_graph_data(subscriptions, range, :sales_data_by_date)
       active_customers = month_graph_data(subscriptions, range, :get_customers_by_date)
       renewal_data = month_graph_data(subscriptions, range, :renewal_data_by_date)
-      { mrr: subcription_month_revenue, active_subscriptions_count: active_subscriptions_count, churn_rate: churn_rate, active_customers: active_customers, customer_lifetime_value: customer_lifetime, revenue_churn: revenue_churn, arr_data: arr_data, mrr_data: mrr_data, refund_data: refund_data, sales_data: sales_data, renewal_data: renewal_data }
+      { mrr: subcription_month_revenue, active_subscriptions_count: active_subscriptions_count,
+        churn_rate: churn_rate, active_customers: active_customers, customer_lifetime_value: customer_lifetime,
+        revenue_churn: revenue_churn, arr_data: arr_data, mrr_data: mrr_data, refund_data: refund_data, sales_data: sales_data, renewal_data: renewal_data }
     end
 
     def get_date_range(duration)
@@ -35,14 +37,16 @@ module Queries
       subscriptions.sum { |subscription| subscription.node.status == status ? subscription.node.lines.edges.count : 0 }
     end
 
-    def get_churn_rate(subscriptions, subscriptions_in_period)
+    def get_churn_rate(subscriptions, range)
+      customer_at_period_start = in_period_subscriptions(subscriptions, range.start - 1.year..range.start - 1.day, 'ACTIVE')
+      cancelled_customer_in_period = in_period_subscriptions(subscriptions, range, 'CANCELLED')
+      (cancelled_customer_in_period * 100) / customer_at_period_start rescue 0
       # (get_subscriptions_count(subscriptions_in_period, 'ACTIVE') * 100) / subscriptions.count
-      3.35
     end
 
     def get_customer_lifetime_value(subscriptions)
       customer_data = subscriptions.group_by { |subscription| subscription.node.customer.id }
-      customer_data.sum { |data| data[1].sum { |subscription| subscription.node.orders.edges.sum{ |order| order.node.total_price_set.presentment_money.amount.to_f} } } / customer_data.size
+      customer_data.sum { |data| data[1].sum { |subscription| subscription.node.orders.edges.sum{ |order| order.node.total_price_set.presentment_money.amount.to_f} } } / customer_data.size  rescue 0
     end
 
     def month_graph_data(subscriptions, range, method)
@@ -67,7 +71,7 @@ module Queries
       subscriptions_in_period = in_period_subscriptions(subscriptions, date.beginning_of_month..date.end_of_month)
       cancelled_subscriptions_revenue = subscriptions_in_period.sum { |subscription| subscription.node.status == 'CANCELLED' ? get_orders_total_amount(subscription) : 0 }
       order_total = subscriptions_in_period.sum { |subscription| get_orders_total_amount(subscription) }
-      (order_total - (order_total - cancelled_subscriptions_revenue)) / order_total rescue 0.0
+      (order_total - (order_total - cancelled_subscriptions_revenue)) / order_total rescue 0
     end
 
     def sales_data_by_date(date, subscriptions)
@@ -95,22 +99,18 @@ module Queries
     end
 
     def get_customers_by_date(date, subscriptions)
-      current_month_subscriptions = in_period_active_subscriptions(subscriptions, date.beginning_of_month..date.end_of_month)
+      current_month_subscriptions = in_period_subscriptions(subscriptions, date.beginning_of_month..date.end_of_month, 'ACTIVE')
       current_month_subscriptions.group_by { |subscription| subscription.node.customer.id }.count
     end
 
     def renewal_data_by_date(date, subscriptions)
-      current_month_subscriptions = in_period_active_subscriptions(subscriptions, date.beginning_of_month..date.end_of_month)
+      current_month_subscriptions = in_period_subscriptions(subscriptions, date.beginning_of_month..date.end_of_month, 'ACTIVE')
       renewed_subscriptions = current_month_subscriptions.sum { |subscription| subscription.node.orders.edges.count > 1 && (orders_in_range(subscription).size > 1) ? 1 : 0  }
-      renewed_subscriptions / current_month_subscriptions.count
+      renewed_subscriptions / current_month_subscriptions.count rescue 0
     end
 
-    def in_period_subscriptions(subscriptions, range)
-      subscriptions.select { |subscription| range.cover?(subscription.node.created_at.to_date) }
-    end
-
-    def in_period_active_subscriptions(subscriptions, range)
-      subscriptions.select { |subscription| range.cover?(subscription.node.created_at.to_date) && subscription.node.status == 'ACTIVE' }
+    def in_period_subscriptions(subscriptions, range, status = nil)
+      subscriptions.select { |subscription| range.cover?(subscription.node.created_at.to_date && (status ? subscription.node.status == status : true)) }
     end
 
     def orders_in_range(subscription)
