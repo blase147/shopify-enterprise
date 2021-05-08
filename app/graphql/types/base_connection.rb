b9aa4da8618a6d3c18d50e34ff1a79d146d8f0ed
Khurram <soft.qureshi@gmail.com>
2021-05-08 12:14:10 +0500

    add pagination to apis

--- app/graphql/types/base_connection.rb
+++ app/graphql/types/base_connection.rb
@@ -2,6 +2,6 @@ class Types::BaseConnection < GraphQL::Types::Relay::BaseConnection
   field :total_count, Integer, null: false
 
   def total_count
-    object.nodes&.count
+    object.items&.count
   end
 end
