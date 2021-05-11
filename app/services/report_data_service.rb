49620ae93e703ceb0d650de7a5bcac5eafaf0136
Khurram <soft.qureshi@gmail.com>
2021-05-11 17:14:05 +0500

    show last day data on dashboard report

--- app/services/report_data_service.rb
+++ app/services/report_data_service.rb
@@ -9,7 +9,7 @@ class ReportDataService
   def get_date_range(duration)
     case duration
     when 'daily'
-      Date.today..Date.today
+      (Date.today - 1.day)..Date.today
     else
       Date.today - instance_eval(duration.downcase.split(' ').join('.'))..Date.today
     end
