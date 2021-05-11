cd608263c2efd802bdc4344216bf40b2e38518fc
Khurram <soft.qureshi@gmail.com>
2021-05-11 12:40:32 +0500

    fix for seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,15 +13,15 @@ shop.smarty_messages.delete_all
 shop.smarty_cancellation_reasons.delete_all
 shop.smarty_variables.delete_all
 # Billing Update
-shop.smarty_messages.create(title: 'Modify Order - Billing Info', body: 'Your current preferred payment method is:/n{{card_brand - card_last4}}/nExpiration {{card_exp_month/card_exp_year}}/nTo update your payment info, please check the email we just sent to your email address on file.')
+shop.smarty_messages.create(title: 'Modify Order - Billing Info', body: 'Your current preferred payment method is:\n{{card_brand - card_last4}}\nExpiration {{card_exp_month/card_exp_year}}\nTo update your payment info, please check the email we just sent to your email address on file.')
 
 # Delay Order
-shop.smarty_messages.create(title: 'Delay Order - Invalid Option', body: 'Oops, it seems you entered an incorrect answer, please choose a valid option:/n1. 1w (Delay 1 week)/n2. 2w (Delay 2 weeks)/n3. 3w (Delay 3 weeks)')
+shop.smarty_messages.create(title: 'Delay Order - Invalid Option', body: 'Oops, it seems you entered an incorrect answer, please choose a valid option:\n1. 1w (Delay 1 week)\n2. 2w (Delay 2 weeks)\n3. 3w (Delay 3 weeks)')
 
 shop.smarty_messages.create(title: 'Delay Order - Failure', body: 'We were unable to delay your upcoming order of {{subscription_title}} scheduled for
 {{subscription_charge_date}} . Please try again or contact support at {{shop_email}}')
 
-shop.smarty_messages.create(title: 'Delay Order - Options', body: 'Please choose from the following options to delay your upcoming order:/n1. 1w (Delay 1 week)/n2. 2w (Delay 2 weeks)/n3. 3w (Delay 3 weeks)')
+shop.smarty_messages.create(title: 'Delay Order - Options', body: 'Please choose from the following options to delay your upcoming order:\n1. 1w (Delay 1 week)\n2. 2w (Delay 2 weeks)\n3. 3w (Delay 3 weeks)')
 
 shop.smarty_messages.create(title: 'Delay Order - Success', body: "Awesome!, you've pushed back this order by {{delay_weeks}} week(s). It will process on
 {{subscription_charge_date}}")
@@ -39,7 +39,7 @@ shop.smarty_messages.create(title: 'Skip Order - Success', body: "Awesome! Youâ€
 # Edit Quantity
 shop.smarty_messages.create(title: 'Edit quantity - Cancel', body: "Alright, the quantity of your order for {{line_item_name}} remains {{line_item_qty}} pc(s)")
 shop.smarty_messages.create(title: 'Edit quantity - Confirm', body: "Hey {{first_name}}! confirm you want to change the quantity of your order for {{line_item_name}}? If so, please reply 'Y' to confirm or 'N' to ignore.")
-shop.smarty_messages.create(title: 'Edit quantity - Conditional', body: "Your order for {{subscription_title}} has the following line items:/n{{line_item_list}}/nPlease respond with the ID of the line item whose quantity you want to change.")
+shop.smarty_messages.create(title: 'Edit quantity - Conditional', body: "Your order for {{subscription_title}} has the following line items:\n{{line_item_list}}\nPlease respond with the ID of the line item whose quantity you want to change.")
 shop.smarty_messages.create(title: 'Edit quantity - conditional_response', body: "Got it! Please reply with the new quantity for {{line_item_name}}")
 shop.smarty_messages.create(title: 'Edit quantity - conditional_response_failure', body: "Oops, it seems you entered an incorrect answer, please choose a valid option")
 shop.smarty_messages.create(title: 'Edit quantity - Success', body: "Awesome! Weâ€™ve changed the quantity of {{line_item_name}}. It will be reflected in your next delivery.")
@@ -49,7 +49,7 @@ shop.smarty_messages.create(title: 'Edit quantity- Invalid Option', body: "Oops,
 # Cancelation
 shop.smarty_messages.create(title: 'End Subscription - confirmation', body: "Confirm you want to cancel your subscription to order {{subscription_title}}? If so, please reply 'Y' to confirm or 'N' to ignore")
 shop.smarty_messages.create(title: 'End Subscription - Cancel', body: "OK, ignored.If you need anything else, let us know. You can reply with 'INFO' or email us at {{shop_email}}")
-shop.smarty_messages.create(title: 'End Subscription - Cancellation Reason', body: "Ok {{first_name}} - before we end your subscription, which of the following best describes why you'd like to cancel? /n{{cancellation_reasons}}")
+shop.smarty_messages.create(title: 'End Subscription - Cancellation Reason', body: "Ok {{first_name}} - before we end your subscription, which of the following best describes why you'd like to cancel? \n{{cancellation_reasons}}")
 shop.smarty_messages.create(title: 'End Subscription - invalid option', body: "Oops, it seems you entered an incorrect answer, please choose a valid option. Reply 'Y' to confirm or 'N' to ignore")
 shop.smarty_messages.create(title: 'End Subscription - Winback Swap', body: "Do you want to swap a product for your current subscription (SWAP) or continue with the cancellation of your subscription (CONTINUE) ?")
 shop.smarty_messages.create(title: 'End Subscription - Winback Skip', body: "Do you want to skip a product for your current subscription (SKIP) or continue with the cancellation of your subscription (CONTINUE) ?")
