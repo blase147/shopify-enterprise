c9da86bfa9e44220f3946528a48d7ccfbfb1a514
Khurram <soft.qureshi@gmail.com>
2021-05-21 19:08:53 +0500

    add analytics for customers

--- app/graphql/queries/fetch_customer_insights.rb
+++ app/graphql/queries/fetch_customer_insights.rb
@@ -3,18 +3,27 @@ module Queries
     type Types::CustomerInsightReportType, null: false
 
     def resolve
-      subscriptions = ReportService.new.all_subscriptions
-      range = Date.today - 1.year..Date.today
+      all_subscriptions = ReportService.new.all_subscriptions
+      range = Date.today - 30.days..Date.today
+      all_subscription_report = CustomerInsightReportService.new(current_shop, subscriptions, range)
+      subscriptions = all_subscription_report.in_period_subscriptions(all_subscriptions, range)
+      previous_day_subscriptions = all_subscription_report.in_period_subscriptions(all_subscriptions, Date.today - 1.day..Date.today - 1.day)
+      current_day_subscriptions = all_subscription_report.in_period_subscriptions(all_subscriptions, Date.today..Date.today)
       report = CustomerInsightReportService.new(current_shop, subscriptions, range)
-      previous_day = CustomerInsightReportService.new(current_shop, subscriptions, Date.today - 1.day..Date.today - 1.day)
-      current_day = CustomerInsightReportService.new(current_shop, subscriptions, Date.today..Date.today)
+      previous_day = CustomerInsightReportService.new(current_shop, previous_day_subscriptions, Date.today - 1.day..Date.today - 1.day)
+      current_day = CustomerInsightReportService.new(current_shop, current_day_subscriptions, Date.today..Date.today)
       swap_count = report.percentage(previous_day.swap_count, current_day.swap_count, report.swap_count)
       skip_count = report.percentage(previous_day.skip_count, current_day.skip_count, report.skip_count)
       restart_count = report.percentage(previous_day.restart_count, current_day.restart_count, report.restart_count)
       skip_customers = report.month_graph_data(:active_vs_churned_skip_data)
       swap_customers = report.month_graph_data(:active_vs_churned_swap_data)
-      { swap_count: swap_count, skip_count: skip_count, restart_count: restart_count, skip_customers: skip_customers,
-        swap_customers: swap_customers }
+      sales_per_charge = report.percentage(previous_day.sales_per_charge, current_day.sales_per_charge, report.sales_per_charge)
+      customers_count = report.percentage(previous_day.sub_customers_count, current_day.sub_customers_count, report.sub_customers_count)
+      charge_per_customer = report.percentage(previous_day.charge_per_customer, current_day.charge_per_customer, report.charge_per_customer)
+      total_churn = report.percentage(previous_day.churn_rate, current_day.churn_rate, report.churn_rate)
+      { customers_count: customers_count, sales_per_charge: sales_per_charge, charge_per_customer: charge_per_customer,
+        total_churn: total_churn, swap_count: swap_count, skip_count: skip_count,
+        restart_count: restart_count, skip_customers: skip_customers, swap_customers: swap_customers }
     end
   end
 end
--- app/services/customer_insight_report_service.rb
+++ app/services/customer_insight_report_service.rb
@@ -1,6 +1,7 @@
 class CustomerInsightReportService
-  def initialize(shop, range)
+  def initialize(shop, subscriptions, range)
     @shop = shop
+    @subscriptions = subscriptions
     @range = range
   end
 
@@ -50,5 +51,35 @@ class CustomerInsightReportService
     @shop.customers.joins(:subscription_logs)
          .where(customers: { status: status }, subscription_logs: { action_type: action })
          .where('subscription_logs.created_at::date BETWEEN ? AND ?', range.first, range.last).count
+  end
+
+  def sales_per_charge
+    @subscriptions.sum { |subscription| get_orders_total_amount(subscription) } / orders_count rescue 0
+  end
+
+  def charge_per_customer
+    @subscriptions.sum { |subscription| get_orders_total_amount(subscription) } / sub_customers_count rescue 0
+  end
+
+  def orders_count
+    @subscriptions.sum { |subscription| subscription.node.orders.edges.count }
+  end
+
+  def sub_customers_count
+    @subscriptions.group_by { |subscription| subscription.node.customer.id }.count
+  end
+
+  def churn_rate
+    customer_at_period_start = in_period_subscriptions(@subscriptions, @range.first - 1.year..@range.first - 1.day, 'ACTIVE').count
+    cancelled_customer_in_period = in_period_subscriptions(@subscriptions, @range, 'CANCELLED').count
+    (cancelled_customer_in_period * 100) / customer_at_period_start rescue 0
+  end
+
+  def in_period_subscriptions(subscriptions, range, status = nil)
+    subscriptions.select { |subscription| range.cover?(subscription.node.created_at.to_date) && (status ? subscription.node.status == status : true) }
+  end
+
+  def get_orders_total_amount(subscription)
+    subscription.node.orders.edges.sum { |order| order.node.total_price_set.presentment_money.amount.to_f.round(2) }
   end
 end
