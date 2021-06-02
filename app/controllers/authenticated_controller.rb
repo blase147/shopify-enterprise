577819affe67a4083020e67d367f2e3e2c7a1986
Khurram <soft.qureshi@gmail.com>
2021-06-02 11:57:17 +0500

    fixes for analytics

--- app/controllers/authenticated_controller.rb
+++ app/controllers/authenticated_controller.rb
@@ -7,7 +7,7 @@ class AuthenticatedController < ApplicationController
 
   def current_shop
     return @current_shop if @current_shop.present?
-    
+
     shop = ShopifyAPI::Shop.current
     @current_shop = Shop.find_by(shopify_domain: shop.myshopify_domain)
   end
