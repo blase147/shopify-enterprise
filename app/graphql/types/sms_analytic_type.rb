cb80cbc5441837594605b4d0668359f1068358a3
Khurram <soft.qureshi@gmail.com>
2021-06-03 15:49:37 +0500

    add cancel count to sms analytics

--- app/graphql/types/sms_analytic_type.rb
+++ app/graphql/types/sms_analytic_type.rb
@@ -2,6 +2,7 @@ module Types
   class SmsAnalyticType < Types::BaseObject
     field :swap_count, Types::GraphValueType, null: true
     field :skip_count, Types::GraphValueType, null: true
+    field :cancel_count, Types::GraphValueType, null: true
     field :delay_count, Types::GraphValueType, null: true
     field :one_time_revenue, Types::GraphValueType, null: true
     field :opt_out_messages, Types::GraphValueType, null: true
