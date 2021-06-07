ce76bc66717aeebb4ca4b9da022d1b1887cdb2e8
Khurram Qureshi <khurram@codility.co>
2021-06-07 21:32:27 +0500

    Temp fix

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -36,7 +36,7 @@ class Customer < ApplicationRecord
 
   def activation_email
     email_notification = shop.setting.email_notifications.find_by_name "Subscription Activation"
-    EmailService::Send.new(email_notification).send_email({customer: self}) unless email_notification.nil?
+    EmailService::Send.new(email_notification).send_email({customer: self}) if email_notification.present? && email_notification.setting.shop
   end
 
   def name
