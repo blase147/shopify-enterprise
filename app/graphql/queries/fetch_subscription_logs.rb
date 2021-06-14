8e411aaddc0207b417a66b1c7b0489c367119576
Khurram Qureshi <khurram@codility.co>
2021-06-14 14:58:06 +0500

    Applied pagination on subscription logs

--- app/graphql/queries/fetch_subscription_logs.rb
+++ app/graphql/queries/fetch_subscription_logs.rb
@@ -1,10 +1,11 @@
 module Queries
   class FetchSubscriptionLogs < Queries::BaseQuery
     type Types::LogType, null: false
+    argument :page, String, required: false
 
-    def resolve
-      subscription_logs = current_shop.subscription_logs.order(created_at: :desc).limit 50
-      {subscription_logs: subscription_logs}
+    def resolve(page)
+      subscription_logs = current_shop.subscription_logs.order(created_at: :desc).page page[:page]
+      {subscription_logs: subscription_logs, total_count: current_shop.subscription_logs.count, total_pages: subscription_logs.total_pages, page_number: page[:page]}
     end
   end
 end
