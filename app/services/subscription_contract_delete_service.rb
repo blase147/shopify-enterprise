db1df6270cc0858fd4b567d22f0ac22a0da1e9aa
Khurram <soft.qureshi@gmail.com>
2021-06-15 17:34:22 +0500

    fix log message

--- app/services/subscription_contract_delete_service.rb
+++ app/services/subscription_contract_delete_service.rb
@@ -33,7 +33,7 @@ class SubscriptionContractDeleteService < GraphqlService
           customer.shop.subscription_logs.cancel.create(customer_id: customer.id, product_name: product.title, note: note, description: description, amount: amount, product_id: product.id)
         end
       else
-        description = customer.name+",just restart,"+product.title
+        description = customer.name+",just restarted,"+product.title
         if @type == "sms"
           customer.shop.subscription_logs.sms.restart.create(customer_id: customer.id, product_name: product.title, note: note, description: description, amount: amount, product_id: product.id)
         else
