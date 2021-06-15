3f20de6ad5122291513b84bd7dece06a3713d648
Khurram <soft.qureshi@gmail.com>
2021-06-15 19:38:36 +0500

    fixes

--- app/javascript/components/layout/Footer.js
+++ app/javascript/components/layout/Footer.js
@@ -4,11 +4,11 @@ import { FooterHelp, Link } from '@shopify/polaris';
 const Footer = () => {
   return (
     <FooterHelp>
-      <Link target="_blank" url={process.env.APP_TYPE=="public"?"https://www.chargezen.co/term-of-use":"#"}>
+      <Link target="_blank" rel="noopener noreferrer" url={process.env.APP_TYPE=="public"?"https://www.chargezen.co/term-of-use":"#"}>
         Terms and Conditions
       </Link>
       <span> • </span>
-      <Link target="_blank" url={process.env.APP_TYPE=="public"?"https://www.chargezen.co/privacypolicy":"#"}>
+      <Link target="_blank" rel="noopener noreferrer"  url={process.env.APP_TYPE=="public"?"https://www.chargezen.co/privacypolicy":"#"}>
         Privacy Policy
       </Link>
     </FooterHelp>
