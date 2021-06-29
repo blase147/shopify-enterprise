acf0e20cb14da9d78c79ebb39c8da7e1faff51d7
Khurram <soft.qureshi@gmail.com>
2021-06-29 16:03:50 +0500

    complete design 3 gicen by jerry

--- db/schema.rb
+++ db/schema.rb
@@ -367,6 +367,7 @@ ActiveRecord::Schema.define(version: 2021_06_16_080257) do
     t.boolean "renewal_reminder", default: false
     t.boolean "skip_update_next_charge", default: false
     t.boolean "one_time_upsells", default: false
+    t.boolean "failed_renewal", default: false
     t.boolean "cancel_reactivate_subscription", default: false
     t.boolean "edit_quantity", default: false
     t.boolean "cancel_subscription", default: false
@@ -376,7 +377,6 @@ ActiveRecord::Schema.define(version: 2021_06_16_080257) do
     t.string "renewal_duration"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.boolean "failed_renewal"
     t.boolean "opt_in", default: false
     t.boolean "swap_product", default: false
     t.boolean "update_billing", default: false
