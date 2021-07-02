1c578130dcc045edff8a262d666e04de948239b6
Khurram <soft.qureshi@gmail.com>
2021-07-02 16:34:31 +0500

    fetch selling plan change

--- app/graphql/queries/fetch_selling_plan_group.rb
+++ app/graphql/queries/fetch_selling_plan_group.rb
@@ -4,7 +4,11 @@ module Queries
     argument :id, ID, required: true
 
     def resolve(id:)
-      current_shop.selling_plan_groups.find(id)
+      if id.include?('gid://shopify')
+        current_shop.selling_plan_groups.find_by_shopify_id(id)
+      else
+        current_shop.selling_plan_groups.find(id)
+      end
     end
   end
 end
