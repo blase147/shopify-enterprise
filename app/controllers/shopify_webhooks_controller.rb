bc98f943dd07e42168430094862c225f22462776
Khurram <soft.qureshi@gmail.com>
2021-07-08 14:37:37 +0500

    add webhook to destroy shop if app uninstall on public app

---
+++ app/controllers/shopify_webhooks_controller.rb
@@ -0,0 +1,12 @@
+class ShopifyWebhooksController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  include ShopifyApp::WebhookVerification
+
+  def app_uninstalled
+    if ENV['APP_TYPE'] == 'public'
+      shop = Shop.where(shopify_domain: params[:domain])
+      shop.destroy if shop.present?
+    end
+    head :no_content
+  end
+end
