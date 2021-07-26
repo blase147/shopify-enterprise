c3a587776f0817207682a6f9005624d039277720
Khurram <soft.qureshi@gmail.com>
2021-07-26 11:11:59 +0500

    add logs for crons

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,13 +1,16 @@
+set :output, "log/cron_log.log"
+require_relative './environment'
+
 every 1.days do
   rake 'subscriptions:attemp_billing'
   rake 'subscriptions:retries_attemp_billing'
 end
 
-every 1.minute do
+every 1.minutes do
   rake 'subscriptions:expire_conversation'
 end
 
-every 10.minute do
+every 10.minutes do
   rake 'subscriptions:opt_in_success'
 end
 
