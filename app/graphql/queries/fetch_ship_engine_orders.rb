d4e7e8dc62a96087c6c9301f044d7bcedc652229
Khurram <soft.qureshi@gmail.com>
2021-08-06 16:11:46 +0500

    remove offset attributes

--- app/graphql/queries/fetch_ship_engine_orders.rb
+++ app/graphql/queries/fetch_ship_engine_orders.rb
@@ -1,11 +1,11 @@
 module Queries
   class FetchShipEngineOrders < Queries::BaseQuery
     type Types::ShipEngineOrderCollectionType, null: false
-    argument :offset_attributes, Types::Input::OffsetAttributes, required: false
+    # argument :offset_attributes, Types::Input::OffsetAttributes, required: false
 
     def resolve(**args)
-      offset_params = args[:offset_attributes].to_h
-      ship_engine_orders = current_shop.ship_engine_orders.order(created_at: :desc).limit(offset_params[:limit]).offset(offset_params[:offset])
+      # offset_params = args[:offset_attributes].to_h
+      ship_engine_orders = current_shop.ship_engine_orders.order(created_at: :desc)#.limit(offset_params[:limit]).offset(offset_params[:offset])
       { ship_engine_orders: ship_engine_orders, total_count: current_shop.ship_engine_orders.count }
     end
   end
