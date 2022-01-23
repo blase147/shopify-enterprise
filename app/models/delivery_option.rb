e2569b4d866db04de89352c6a87ab847bf5e9755
Zaheer <zaheer.aws@gmail.com>
2022-01-23 14:07:06 +0500

    Updated scription to provide complete delivery date according to next availability

--- app/models/delivery_option.rb
+++ app/models/delivery_option.rb
@@ -5,7 +5,20 @@ class DeliveryOption < ApplicationRecord
     {
       id: id,
       delivery_option: delivery_option,
-      settings: settings.nil? ? [] : JSON.parse(settings)
+      settings: settings.nil? ? [] : setup_settings(settings)
     }
+  end
+
+  def setup_settings(settings)
+    sets = []
+    JSON.parse(settings).each do |set|
+      if Date.today.strftime('%Y%m%d') < Date.today.at_beginning_of_week(set['cutoff_day'].to_sym).strftime('%Y%m%d')
+        set['date'] = Date.today.at_beginning_of_week(set['delivery'].to_sym).strftime( '%A %d %b %Y' )
+      else
+        set['date'] = Date.today.next_week(set['delivery'].to_sym).strftime( '%A %d %b %Y' )
+      end
+      sets << set
+    end
+    sets
   end
 end
