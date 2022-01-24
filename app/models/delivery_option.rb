1c632d9a6a756df3e40c2c0a64fd814bfa5b6224
Zaheer <zaheer.aws@gmail.com>
2022-01-24 14:51:00 +0500

    Fixes and delivery updates

--- app/models/delivery_option.rb
+++ app/models/delivery_option.rb
@@ -11,13 +11,32 @@ class DeliveryOption < ApplicationRecord
 
   def setup_settings(settings)
     sets = []
-    JSON.parse(settings).each do |set|
-      if Date.today.strftime('%Y%m%d') < Date.today.at_beginning_of_week(set['cutoff_day'].to_sym).strftime('%Y%m%d')
-        set['date'] = Date.today.at_beginning_of_week(set['delivery'].to_sym).strftime( '%A %d %b %Y' )
-      else
-        set['date'] = Date.today.next_week(set['delivery'].to_sym).strftime( '%A %d %b %Y' )
+    next_week_used = false
+    week = 1
+    stop = 1
+    while (stop <= delivery_option) do
+      JSON.parse(settings).each do |set|
+        if stop <= delivery_option
+          if week == 1
+            if Date.today.strftime('%Y%m%d') < Date.today.at_beginning_of_week(set['cutoff_day'].to_sym).strftime('%Y%m%d')
+              set['date'] = Date.today.at_beginning_of_week(set['delivery'].to_sym).strftime( '%A %d %b %Y' )
+            else
+              next_week_used = true
+              set['date'] = Date.today.next_week(set['delivery'].to_sym).strftime( '%A %d %b %Y' )
+            end
+          else
+            date = Date.today if week == 2 && next_week_used == false
+            date = Date.today.next_week if week == 3 || (next_week_used && week == 2)
+            date = Date.today.next_week.next_week if week == 4 || (next_week_used && week == 3)
+            date = Date.today.next_week.next_week.next_week if week == 5 || (next_week_used && week == 4)
+            date = Date.today.next_week.next_week.next_week if (next_week_used && week == 5)
+            set['date'] = date.next_week(set['delivery'].to_sym).strftime( '%A %d %b %Y' )
+          end
+          sets << set
+        end
+        stop += 1
       end
-      sets << set
+      week += 1
     end
     sets
   end
