1f9e487980bc246b410bca2c1765eef5c4d1e227
PHP <alaw3813@gmail.com>
2022-08-09 22:05:43 +0530

    changed the expected_delivery_date from note to note_attributes

--- app/workers/preorder_update_worker.rb
+++ app/workers/preorder_update_worker.rb
@@ -8,9 +8,12 @@ class PreorderUpdateWorker
                 shopify_order =  ShopifyAPI::Order.find(order["node"]["id"].split("/")&.last&.to_i)
                 products = []
                 shopify_order&.line_items.map{|p| products << p&.product_id&.to_s }
-                note_hash = JSON.parse(shopify_order&.note) rescue {}
-                note_hash = note_hash.transform_keys(&:downcase)
-                expected_delivery_date = note_hash["expected delivery date"]
+                note_hash = shopify_order&.note_attributes&.first
+                if note_hash&.name == "Expected Delivery Date"
+                    expected_delivery_date = note_hash.value
+                else
+                    expected_delivery_date = nil
+                end 
                 week = expected_delivery_date.nil? ? "0" : expected_delivery_date&.to_date&.strftime("%W")&.to_s
                 WorldfarePreOrder.upsert({shop_id: contract.shop_id,customer_id: contract.shopify_customer_id, week:  week,products: products&.to_s, shopify_contract_id: contract.shopify_id, expected_delivery_date: expected_delivery_date, order_id: shopify_order&.id.to_s}, unique_by: :order_id )
             end
