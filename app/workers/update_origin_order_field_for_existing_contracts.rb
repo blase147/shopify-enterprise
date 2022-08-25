9e410ffea42fd2b921d7a0e8ffdf0bf3205ca08e
PHP <alaw3813@gmail.com>
2022-08-25 19:59:32 +0530

    fix exclude some meals from the origin_order_meals field

--- app/workers/update_origin_order_field_for_existing_contracts.rb
+++ app/workers/update_origin_order_field_for_existing_contracts.rb
@@ -2,7 +2,7 @@ class UpdateOriginOrderFieldForExistingContracts
     include Sidekiq::Worker
     def perform
         contracts = CustomerSubscriptionContract.all
-        exclude = ["8 meals - Medium", "6 meals - Small", "4 meals - Trial", "12 meals - Large", "20 meals - Family"]
+        exclude = ["8 meals - medium", "6 meals - small", "4 meals - trial", "12 meals - large", "20 meals - family"]
         contracts&.each do |contract|
             orders = []
             if contract.api_data["origin_order"]["line_items"].present?
