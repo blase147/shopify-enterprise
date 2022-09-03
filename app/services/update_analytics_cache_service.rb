e8c0e712b47758b7d6c4ac1f6b8495079fde82db
ecomtargetllc <65632142+ecomtargetllc@users.noreply.github.com>
2022-09-03 07:41:26 -0700

    define current_shop as local variable

--- app/services/update_analytics_cache_service.rb
+++ app/services/update_analytics_cache_service.rb
@@ -1,4 +1,10 @@
 class UpdateAnalyticsCacheService < ApplicationService
+   def current_shop
+    return @current_shop if @current_shop.present?
+
+    @current_shop = Shop.find_by(shopify_domain: params[:shop])
+  end
+  
   def call(shop)
     start_date, end_date = default_date
 
