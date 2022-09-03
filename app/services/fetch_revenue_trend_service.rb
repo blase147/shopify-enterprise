78c0e841b99ea52498a63b3ddd1e7a6c4dea9e44
ecomtargetllc <65632142+ecomtargetllc@users.noreply.github.com>
2022-09-03 06:31:23 -0700

    Create fetch_revenue_trend_service.rb

---
+++ app/services/fetch_revenue_trend_service.rb
@@ -0,0 +1,18 @@
+class FetchRevenueTrendService < ApplicationService
+  include Redisable
+
+  def call(shop, start_date:, end_date:, refresh: false)
+    data_key = analytics_key(shop, start_date, end_date)
+    data = redis.get(data_key)
+    return JSON.parse(data) if data && refresh == false
+
+    revenue_trend = ComputeRevenueTrendService.new.call(shop, start_date: start_date, end_date: end_date)
+    redis.set(data_key, revenue_trend.to_json, ex: 15.minutes)
+
+    revenue_trend
+  end
+
+  def analytics_key(shop, s_date, e_date)
+    "#{shop.id}:#{s_date}:#{e_date}"
+  end
+end
