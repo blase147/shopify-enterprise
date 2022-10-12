e947249e7bc8c92ae2034d4b84ea7524b142155b
Chargezen Inc <65632142+ecomtargetllc@users.noreply.github.com>
2022-10-12 02:25:58 -0700

    test schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -16,9 +16,9 @@ end
 #   rake 'subscriptions:expire_conversation'
 # end
 
-# every 10.minutes do
-#   rake 'subscriptions:opt_in_success'
-# end
+ every 10.minutes do
+   rake 'subscriptions:opt_in_success'
+ end
 
 every 1.days do
   rake 'subscriptions:renewal_reminder'
