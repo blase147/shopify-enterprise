160d12bd751244d0b8cacd99f71e9caad76ab713
Chargezen Inc <65632142+ecomtargetllc@users.noreply.github.com>
2022-10-13 08:28:25 -0700

    Update pre_order_email_notification_worker.rb

--- app/workers/pre_order_email_notification_worker.rb
+++ app/workers/pre_order_email_notification_worker.rb
@@ -9,5 +9,4 @@ class PreOrderEmailNotificationWorker
     params = {contract_id: contract_id}
     message = "#{e.message} from #{e.backtrace.first}"
     SiteLog.create(log_type: SiteLog::TYPES[:sidekiq_job_failure], message: message, params: params)
-  end
 end
