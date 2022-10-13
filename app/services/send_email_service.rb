7028fdeecb0d738111dfad147e99bab727d2a8ad
PHP <alaw3813@gmail.com>
2022-10-13 20:19:00 +0530

    completed fill preorder email

--- app/services/send_email_service.rb
+++ app/services/send_email_service.rb
@@ -75,21 +75,28 @@ class SendEmailService
 
         week_number = Date.current.cweek
         shop = contract.shop
+        shop&.connect
         meals_on_plan = contract.subscription.split[0].to_i
-
         pre_order = WorldfarePreOrder.find_by(shopify_contract_id: contract.shopify_id, week: week_number)
+        order = ShopifyAPI::Order.find(pre_order&.order_id) rescue nil
+        expected_order_delivery = CalculateOrderDelivery.new(contract, contract.shop.id).expected_delivery_of_order(order&.created_at)
 
-        pre_order_products = JSON.parse(pre_order.products) rescue []
-       
+        products = []
+        
+        order&.line_items&.each do |product|
+            products << product&.title unless product&.title&.downcase&.include? "meals"
+        end
+
         email_notification = shop.setting.email_notifications.find_by_name "Fill PreOrder"
         customer_object = {customer: contract}
-        if EmailService::Send.new(email_notification).send_email(customer_object)
+        sent= EmailService::Send.new(email_notification).send_email({customer: contract, delivery_date: expected_order_delivery&.strftime("%Y-%m-%d"), delivery_day: pre_order&.delivery_day_week_specific, products:  products.to_sentence}) if email_notification.present? && contract.shop.setting.email_service.present?
+        if sent
           SiteLog.create(log_type: SiteLog::TYPES[:email_success], message: "PreOrderEmailNotification sent")
         else
           SiteLog.create(log_type: SiteLog::TYPES[:email_failure], params: {id: contract.id, shopify_id: contract.shopify_customer_id })
         end
         
-        sent= EmailService::Send.new(email_notification).send_email({customer: contract}) if email_notification.present? && contract.shop.setting.email_service.present?
+        
         return sent
     end
 end
