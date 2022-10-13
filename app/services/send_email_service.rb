40def74ee29b1f1e3f68db39959836de2be1535d
Chargezen Inc <65632142+ecomtargetllc@users.noreply.github.com>
2022-10-13 05:39:27 -0700

    add template for fill preorder

--- app/services/send_email_service.rb
+++ app/services/send_email_service.rb
@@ -69,4 +69,27 @@ class SendEmailService
         sent= EmailService::Send.new(email_notification).send_email({customer: contract}) if email_notification.present? && contract.shop.setting.email_service.present?
         return sent
     end
+     def send_fill_preorder_email(contract_id)
+        contract = CustomerSubscriptionContract.find_by_id contract_id
+        contract ||= CustomerSubscriptionContract.find_by(shopify_id: contract_id)
+
+        week_number = Date.current.cweek
+        shop = contract.shop
+        meals_on_plan = contract.subscription.split[0].to_i
+
+        pre_order = WorldfarePreOrder.find_by(shopify_contract_id: contract.shopify_id, week: week_number)
+
+        pre_order_products = JSON.parse(pre_order.products) rescue []
+       
+        email_notification = shop.setting.email_notifications.find_by_name "Fill PreOrder"
+        customer_object = {customer: contract}
+        if EmailService::Send.new(email_notification).send_email(customer_object)
+          SiteLog.create(log_type: SiteLog::TYPES[:email_success], message: "PreOrderEmailNotification sent")
+        else
+          SiteLog.create(log_type: SiteLog::TYPES[:email_failure], params: {id: contract.id, shopify_id: contract.shopify_customer_id })
+        end
+        
+        sent= EmailService::Send.new(email_notification).send_email({customer: contract}) if email_notification.present? && contract.shop.setting.email_service.present?
+        return sent
+    end
 end
