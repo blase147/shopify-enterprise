7000dd57f2403560e760e4c922902314e458d7da
PHP <alaw3813@gmail.com>
2022-11-23 15:05:16 +0530

    added toast in customer migration and added origin_order check in send_activation email

--- app/services/send_email_service.rb
+++ app/services/send_email_service.rb
@@ -19,11 +19,14 @@ class SendEmailService
         if contract.present?
             email_notification = contract.shop.setting.email_notifications.find_by_name "Subscription Activation"
             products = []
-            order_number = contract.api_data["origin_order"]["id"]&.split("/").last
-            contract&.origin_order_meals&.each do |product|
-                products << product["node"]["product"]["title"] unless product["node"]["product"]["title"]&.downcase&.include? "meals"
+            order_number = contract.api_data["origin_order"]["id"]&.split("/").last rescue nil
+            if order_number.present?
+                contract&.origin_order_meals&.each do |product|
+                    products << product["node"]["product"]["title"] unless product["node"]["product"]["title"]&.downcase&.include? "meals"
+                end
+                EmailService::Send.new(email_notification).send_email({customer: contract, order_details_first: "Order Number: #{order_number} Meals: #{products.to_sentence}", delivery_date_first: contract.delivery_date }) if email_notification.present? && contract.shop.setting.email_service.present?
+        
             end
-            EmailService::Send.new(email_notification).send_email({customer: contract, order_details_first: "Order Number: #{order_number} Meals: #{products.to_sentence}", delivery_date_first: contract.delivery_date }) if email_notification.present? && contract.shop.setting.email_service.present?
         end
     end
 
@@ -43,10 +46,13 @@ class SendEmailService
         email_notification = contract.shop.setting.email_notifications.find_by_name "Subscription Activation 2 Hours"
         products = []
         order_number = contract.api_data["origin_order"]["id"]&.split("/").last
-        contract&.origin_order_meals&.each do |product|
-            products << product["node"]["product"]["title"] unless product["node"]["product"]["title"]&.downcase&.include? "meals"
+        if order_number.present?
+            contract&.origin_order_meals&.each do |product|
+                products << product["node"]["product"]["title"] unless product["node"]["product"]["title"]&.downcase&.include? "meals"
+            end
+            EmailService::Send.new(email_notification).send_email({customer: contract, order_details_first: "Order Number: #{order_number} Meals: #{products.to_sentence}", delivery_date_first: delivery_date }) if email_notification.present? && contract.shop.setting.email_service.present?
+    
         end
-        EmailService::Send.new(email_notification).send_email({customer: contract, order_details_first: "Order Number: #{order_number} Meals: #{products.to_sentence}", delivery_date_first: delivery_date }) if email_notification.present? && contract.shop.setting.email_service.present?
     end
 
     def send_account_activation_url_email(contract, activation_url)
