e8aa951f2ce08f261c3deeb8729586710666ea3f
PHP <alaw3813@gmail.com>
2023-02-08 18:13:35 +0530

    fixed shopify_domain param in authorize_user_shop api App.js and return current_shop's customer_migration in FetchCustomerModel graphql query

--- app/graphql/queries/fetch_customer_model.rb
+++ app/graphql/queries/fetch_customer_model.rb
@@ -5,7 +5,7 @@ module Queries
     argument :page, String, required: false
 
     def resolve(**args)
-      customer_subscriptions = CustomerModal.all.page(args[:page])
+      customer_subscriptions = current_shop.customer_modals.all.page(args[:page])
       {customer_subscriptions: customer_subscriptions, total_count: customer_subscriptions.count, total_pages: customer_subscriptions.total_pages, page_number: args[:page]}
     end
   end
