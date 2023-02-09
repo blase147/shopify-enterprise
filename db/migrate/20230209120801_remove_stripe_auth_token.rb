530ee94b71580ba53e34606916b89ef615686b4a
PHP <alaw3813@gmail.com>
2023-02-09 17:51:21 +0530

    added stripe_contract_concern, and RemoveStripeAuthToken migration

--- db/migrate/20230209120801_remove_stripe_auth_token.rb
+++ db/migrate/20230209120801_remove_stripe_auth_token.rb
@@ -1,6 +1,6 @@
 class RemoveStripeAuthToken < ActiveRecord::Migration[6.0]
   def change
-    remove_column :customer_modals, :stripe_auth_token_id
+    remove_column :customer_modals, :stripe_contract_token
     drop_table :stripe_auth_tokens
   end
 end
