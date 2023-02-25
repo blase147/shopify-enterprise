24f053758a10054356dc1d4215db1347f2951ecb
Chargezen Inc <65632142+ecomtargetllc@users.noreply.github.com>
2023-02-25 00:20:15 -0800

    Update puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -4,8 +4,8 @@
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum; this matches the default thread size of Active Record.
 #
-min_threads_count =  ENV.fetch("RAILS_MIN_THREADS") { 5 }
-max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 25 }
+min_threads_count =  ENV.fetch("RAILS_MIN_THREADS") { 15 }
+max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 30 }
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
@@ -24,7 +24,7 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 # Workers do not work on JRuby or Windows (both of which do not support
 # processes).
 #
-workers ENV.fetch("WEB_CONCURRENCY") { 250 }
+workers ENV.fetch("WEB_CONCURRENCY") { 300 }
 
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
