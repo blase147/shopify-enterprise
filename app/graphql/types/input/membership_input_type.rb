a428cd273ba273db4e3035ea61ecce641ac05c39
PHP <alaw3813@gmail.com>
2023-03-06 20:44:56 +0530

    added product_images,variant_images, membership type to membership table and also added these fields in create membership form

--- app/graphql/types/input/membership_input_type.rb
+++ app/graphql/types/input/membership_input_type.rb
@@ -5,7 +5,10 @@ module Types
         argument :name, String, required: false
         argument :status, String, required: false
         argument :tag, String, required: false
-        argument :selling_plan_id, [Types::Input::RuleCustomerValueInputType], required: false
+        argument :selling_plan, [Types::Input::RuleCustomerValueInputType], required: false
+        argument :variant_images, [Types::Input::VariantInputType], required: false
+        argument :product_images, [Types::Input::ProductInputType], required: false
+        argument :membership_type, String, required: false
       end
     end
 end
